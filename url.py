9c550d258c0e755e729ad153896f185c279dec69
Qinmin <qinmin0117@outlook.com>
2018-07-16 20:18:16 +0800

    get wiki content

--- url.py
+++ url.py
@@ -25,7 +25,31 @@ if __name__ == "__main__":
     # print(path)
     html_path = os.path.join(path, "html", "main.html")
     # print(html_path)
-    html = open(html_path, mode='rb')
-    bf = BeautifulSoup(html,'html.parser')
+    with open(html_path, mode='rb') as html:
+        html = open(html_path, mode='rb')
+        bf = BeautifulSoup(html,'html.parser')
+
+        gameInfo_L1 = []
+        gameInfo_L2 = []
+        gameInfo_L3 = []
+        gameInfo_list = [gameInfo_L1, gameInfo_L2, gameInfo_L3]
+
+        gameInfo = bf.find("li", class_="wds-tabs__tab")
+        print(gameInfo.find('span').string)
+        gameInfo_L1.append(gameInfo.find('span').string)
+        print(gameInfo_L1) # 游戏资料
+        for span in gameInfo.find('div', class_='wds-dropdown__content').find_all('span'):
+            print(span.string)
+            gameInfo_L2.append(span.string)
+        print(gameInfo_L2) # 游戏功能... 等7个目录
+
+        for div in gameInfo.find_all('div', class_='wds-dropdown-level-2__content'):
+            list_L3 = []
+            for a in div.find_all('a'):
+                list_L3.append(a.string)
+            gameInfo_L3.append(list_L3)
+        print(gameInfo_L3)
+
+    print('End.')
     
 
