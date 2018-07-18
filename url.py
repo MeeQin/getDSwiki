dd2ab466b87572f04a5db82bb7e1b194ca5a651f
qinmin-006646 <qinmin0117@outlook.com>
2018-07-18 17:59:08 +0800

    add article

--- url.py
+++ url.py
@@ -5,7 +5,7 @@
 :FileName: url.py
 :CreateBy: qinmin-006646
 :CreateTime: 2018-07-16
-:Description: get 
+:Description: get Don'tStave wiki information from http://zh.dontstarve.wikia.com/.
 '''
 
 #CreateTime:2018-07-16
@@ -46,8 +46,8 @@ def getMenu(url):
 
     for span in gameInfo.find('div', class_='wds-dropdown__content').find_all('span'):
         gameInfo_L2.append(span.string)
-        gameInfo_Dict[gameInfo_L1].append({span.string : {}}) # {'遊戲資料': [{'遊戲功能': {}, {'角色': {}, ...}]}
-    # print(gameInfo_L2) # 游戏功能... 等7个目录
+        gameInfo_Dict[gameInfo_L1].append({span.string : {}})
+        # {'遊戲資料': [{'遊戲功能': {}, {'角色': {}, ...}]}
 
     index = 0
     for div in gameInfo.find_all('div', class_='wds-dropdown-level-2__content'):
@@ -55,7 +55,8 @@ def getMenu(url):
         for a in div.find_all('a'):
             list_L3.append(a.string)
             # print(a['href'])
-            gameInfo_Dict[gameInfo_L1][index][gameInfo_L2[index]][a.string] = a['href'] # {'遊戲資料': [{'遊戲功能': {'可合成': '/wiki/%E5%90%88%E6%88%90', ...}}]}
+            gameInfo_Dict[gameInfo_L1][index][gameInfo_L2[index]][a.string] = a['href']
+            # {'遊戲資料': [{'遊戲功能': {'可合成': '/wiki/%E5%90%88%E6%88%90', ...}}]}
         index = index +1
 
     return gameInfo_Dict
@@ -68,6 +69,7 @@ if __name__ == "__main__":
 
 
     keyurl = '/wiki/%E5%90%88%E6%88%90'
+    keyword = '可合成'
     # getInfo(keyurl)
     url = main_url + keyurl
     response = requests.get(url = url)
@@ -84,7 +86,26 @@ if __name__ == "__main__":
 
     # get article
     # <article id="WikiaMainContent" class="WikiaMainContent">
-    
+    article = bf.find_all('article', class_='WikiaMainContent')
+    # print(article)
+    path = os.getcwd()
+    filename = keyword + '.html'
+    article_path = os.path.join(path, "html", "wiki", "article", filename)
+    # print(article_path)
+    article_f = open(article_path, mode='w',encoding='utf-8')
+    meta = '''
+    < !doctype html >
+    < html lang = "zh" dir = "ltr" class ="" >
+    < head >
+    < meta http - equiv = "Content-Type" content = "text/html; charset=UTF-8" >
+    < meta name = "viewport" content = "width=device-width, initial-scale=1.0, user-scalable=yes" >
+    < /head >   \n'''
+    # article_f.write(meta)
+    article_s = str(article)
+    article_s = article_s[1:]
+    article_s = article_s[:-1]
+    article_f.write(str(article_s))
+    article_f.close()
 
     print('End.')
     
